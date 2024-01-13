8aef0326a8414a15ad75f7a4b4ece552b4e7725d
diego napan <130240874+diegonabe@users.noreply.github.com>
2024-01-13 16:56:35 -0500

    FIX comments in task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""HolbertonBnB main Flask application.
+"""Starts a Flask web application.
 
-The application listens on host IP 0.0.0.0, port 5000.
+The application listens on 0.0.0.0, port 5000.
 Routes:
-    /hbnb: HBnB home page.
+    /: Displays 'Hello HBNB!'
 """
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Returns 'Hello HBNB!"""
-    return 'Hello HBNB!'
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
